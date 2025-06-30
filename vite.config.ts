caaf483b2bd87294cfcae12094a0151b8bb600e4
Omar Adel <o.adel@walaplus.com>
2025-06-30 16:56:32 +0300

    Update GitHub Actions workflow for static content deployment and adjust Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/omaradel.dev/', // This is crucial for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
