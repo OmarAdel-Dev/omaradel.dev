6b1707f04a737895e18eae3e1e9cd641b01aaa1c
Omar Adel <o.adel@walaplus.com>
2025-06-30 17:02:42 +0300

    Update homepage URL in package.json and adjust base path in vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/omaradel.dev/', // This is crucial for GitHub Pages
+  base: '/', // This is crucial for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
